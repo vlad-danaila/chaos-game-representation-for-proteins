a792a459fe2dfa8c65af7c5f9b5bafd950137413
VLAD RARES DANAILA <vlad.danaila@ro.ibm.com>
2020-06-16 19:16:15 +0300

    gausian fit

--- tobit/tobit.py
+++ tobit/tobit.py
@@ -11,7 +11,7 @@ def pdf(n):
     return ( 1 / math.sqrt(2 * math.pi) ) * t.exp( (-1/2) * (n ** 2) )
 
 def negative_log_likelihood(y, mean, var):
-    return -t.sum(t.log((1 / var) * pdf((y - mean) / var)))
+    return -t.sum(t.log((1 / var) * (pdf((y - mean) / var)) + 1e-8))
 
 
 def read_tensors_from_assay_intervals(intervals: List[p.interval.Interval]):
@@ -23,16 +23,14 @@ def read_tensors_from_assay_intervals(intervals: List[p.interval.Interval]):
     return t.tensor(single_valued, dtype=t.float, device=constants.DEVICE)
 
 if __name__ == '__main__':
-    assay = Assay('', '', [ p.singleton(0), p.singleton(0.5), p.singleton (1), p.singleton(1) ], None)
+    assay = Assay('', '', [ p.singleton(100), p.singleton(150), p.singleton(150) ], None)
     tensors = read_tensors_from_assay_intervals(assay.ic50)
-    mean, var = t.tensor(0, dtype=float, requires_grad=True), t.tensor(1, dtype=float, requires_grad=True)
-    optimizer = t.optim.SGD([mean, var], lr = 1e-3)
+    mean, var = t.tensor(100, dtype=float, requires_grad=True), t.tensor(50, dtype=float, requires_grad=True)
+    optimizer = t.optim.SGD([mean, var], lr = 1e+0)
     for i in range(10_000):
         optimizer.zero_grad()
         log_likelihood = negative_log_likelihood(tensors, mean, var)
         log_likelihood.backward()
         optimizer.step()
-        print(mean, var)
-
-    mean, std = norm.fit(tensors)
+    print(mean, var)
     print('Expected', norm.fit(tensors))
