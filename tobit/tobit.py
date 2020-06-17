42b9b63dd7ea1efd2668743e91f33452c29651d0
VLAD RARES DANAILA <vlad.danaila@ro.ibm.com>
2020-06-18 01:03:54 +0300

    Read censored data

--- tobit/tobit.py
+++ tobit/tobit.py
@@ -19,24 +19,32 @@ def negative_log_likelihood_reparametized(y, delta, gamma):
     return -t.sum(t.log(gamma) + t.log(pdf(gamma * y - delta)))
 
 def read_tensors_from_assay_intervals(intervals: List[p.interval.Interval]):
-    single_valued = []
+    single_valued, right_censored, left_censored = [], [], []
     for interval in intervals:
         # If single valued interval
         if interval.lower == interval.upper:
             single_valued.append(interval.lower)
-    return t.tensor(single_valued, dtype=t.float, device=constants.DEVICE)
+        elif interval.upper == p.inf:
+            right_censored.append(interval.lower)
+        elif interval.lower == 0:
+            left_censored.append(interval.upper)
+        else:
+            raise Exception('Uncensored interval encountered', interval)
+    return t.tensor(single_valued, dtype=t.float, device=constants.DEVICE), \
+           t.tensor(right_censored, dtype=t.float, device=constants.DEVICE), \
+           t.tensor(left_censored, dtype=t.float, device=constants.DEVICE)
 
 def tobit_mean_and_variance(intervals: List[p.interval.Interval]):
-    single_val_tensors = read_tensors_from_assay_intervals(intervals)
-    single_val_mean, single_val_std = single_val_tensors.mean(), single_val_tensors.std(unbiased = False)
-    single_val_tensors = (single_val_tensors - single_val_mean) / single_val_std
+    single_val, right_censored, left_censored = read_tensors_from_assay_intervals(intervals)
+    single_val_mean, single_val_std = single_val.mean(), single_val.std(unbiased = False)
+    single_val = (single_val - single_val_mean) / single_val_std
     mean, std = t.tensor(0, dtype=float, requires_grad=True), t.tensor(1, dtype=float, requires_grad=True)
     optimizer = t.optim.SGD([mean, std], lr=1e-4)
     patience = 5
     for i in range(100_000):
         prev_mean, prev_std = mean.clone(), std.clone()
         optimizer.zero_grad()
-        log_likelihood = negative_log_likelihood(single_val_tensors, mean, std)
+        log_likelihood = negative_log_likelihood(single_val, mean, std)
         log_likelihood.backward()
         optimizer.step()
         early_stop = math.fabs(mean - prev_mean) + math.fabs(std - prev_std) < 1e-8
@@ -50,16 +58,17 @@ def tobit_mean_and_variance(intervals: List[p.interval.Interval]):
     return mean + single_val_mean, std * single_val_std
 
 def tobit_mean_and_variance_reparametrization(intervals: List[p.interval.Interval]):
-    single_val_tensors = read_tensors_from_assay_intervals(intervals)
-    single_val_mean, single_val_std = single_val_tensors.mean(), single_val_tensors.std(unbiased = False)
-    single_val_tensors = (single_val_tensors - single_val_mean) / single_val_std
+    single_val, right_censored, left_censored = read_tensors_from_assay_intervals(intervals)
+    print(single_val, right_censored, left_censored)
+    single_val_mean, single_val_std = single_val.mean(), single_val.std(unbiased = False)
+    single_val = (single_val - single_val_mean) / single_val_std
     delta, gamma = t.tensor(0, dtype=float, requires_grad=True), t.tensor(1, dtype=float, requires_grad=True)
     optimizer = t.optim.SGD([delta, gamma], lr=1e-4)
     patience = 5
     for i in range(100_000):
         prev_delta, prev_gamma = delta.clone(), gamma.clone()
         optimizer.zero_grad()
-        log_likelihood = negative_log_likelihood_reparametized(single_val_tensors, delta, gamma)
+        log_likelihood = negative_log_likelihood_reparametized(single_val, delta, gamma)
         log_likelihood.backward()
         optimizer.step()
         early_stop = math.fabs(delta - prev_delta) + math.fabs(gamma - prev_gamma) < 1e-8
@@ -72,39 +81,26 @@ def tobit_mean_and_variance_reparametrization(intervals: List[p.interval.Interva
     mean, std = delta / gamma, gamma ** -2
     return mean + single_val_mean, std * single_val_std
 
+def plot_gausian(mean, std):
+    x = np.linspace(mean - 3 * std, mean + 3 * std, 1000)
+    plt.plot(x, norm.pdf(x, mean, std))
+
 def gausian_curves_1():
-    ic50 = [ p.singleton(30), p.singleton(30), p.singleton(50) ]
-    mean, std = norm.fit(read_tensors_from_assay_intervals(ic50))
-    x = np.linspace(mean - 3 * std, mean + 3 * std, 100)
-    plt.plot(x, norm.pdf(x, mean, std))
-    # plt.plot(x, norm.cdf(x, mean, std))
-
-    ic50 = [ p.singleton(30), p.singleton(30), p.singleton(60) ]
-    mean, std = norm.fit(read_tensors_from_assay_intervals(ic50))
-    x = np.linspace(mean - 3 * std, mean + 3 * std, 100)
-    plt.plot(x, norm.pdf(x, mean, std))
-    # plt.plot(x, norm.cdf(x, mean, std))
-
-    ic50 = [ p.singleton(30), p.singleton(30), p.singleton(70) ]
-    mean, std = norm.fit(read_tensors_from_assay_intervals(ic50))
-    x = np.linspace(mean - 3 * std, mean + 3 * std, 100)
-    plt.plot(x, norm.pdf(x, mean, std))
-    # plt.plot(x, norm.cdf(x, mean, std))
-
+    for outlier in [50, 60, 70]:
+        ic50 = [p.singleton(30), p.singleton(30), p.singleton(outlier)]
+        mean, std = norm.fit(read_tensors_from_assay_intervals(ic50))
+        x = np.linspace(mean - 3 * std, mean + 3 * std, 100)
+        plt.plot(x, norm.pdf(x, mean, std))
+        # plt.plot(x, norm.cdf(x, mean, std))
     plt.show()
 
-
+def to_numpy(tensor: t.Tensor):
+    return tensor.clone().detach().numpy()
 
 if __name__ == '__main__':
-    # assay = Assay('', '', [ p.singleton(150), p.singleton(100), p.singleton(150) ], None)
-    # print(tobit_mean_and_variance_reparametrization(assay.ic50))
-    # print('Expected', norm.fit(read_tensors_from_assay_intervals(assay.ic50)))
-
-    # gausian_curves_1()
-    # cdf_aprox_plot()
-    log_cdf_gradients_plot()
-
-    # tensor = t.tensor([0, 100, 100, 100, 100, 100, 100], dtype=t.float)
-    # single_val_mean, single_val_std = tensor.mean(), tensor.std(unbiased=False)
-    # tensor = (tensor - single_val_mean) / single_val_std
-    # print(tensor)
+    assay = Assay('', '', [ p.singleton(10), p.singleton(20), p.singleton(30), p.closed(50, p.inf), p.closed(50, p.inf) ], None)
+    mean, std = tobit_mean_and_variance_reparametrization(assay.ic50)
+    print(mean, std)
+    print('Expected', norm.fit(read_tensors_from_assay_intervals(assay.ic50)[0]))
+    plot_gausian(to_numpy(mean), to_numpy(std))
+    plt.show()
