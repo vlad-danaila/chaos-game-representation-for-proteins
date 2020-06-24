Fixed numerical problem - negative log

tobit/log_cdf_aproximation.py
@@ -20,7 +20,9 @@ class LogCdfSoftplusAproximation(Module):
         self.q = Parameter(to_tensor(-2.0691 + random()))
 
     def forward(self, x):
-        return t.sigmoid(self.a * x + self.b) * self.m * t.log(self.n + t.exp(self.p * x + self.q))
+        # the max is for numerical stability
+        log_arg = t.max(self.n + t.exp(self.p * x + self.q), t.tensor(1e-10, dtype=t.float))
+        return t.sigmoid(self.a * x + self.b) * self.m * t.log(log_arg)
 
 class LogCdfEnsembleAproximation(Module):
 
@@ -60,16 +62,16 @@ def fit_softplus_to_log_1_minus_cdf(load_from_chekpoint = False):
     t.save(model.state_dict(), constants.LOG_CDF_APROXIMATION_CHECKPOINT)
 
 def log_cdf_plot(aporx_function):
-    x = t.tensor(np.linspace(-50, 50, 1000), dtype=t.float64, requires_grad=False)
+    x = t.tensor(np.linspace(-80, 80, 1000), dtype=t.float64, requires_grad=False)
     plt.plot(x.clone().detach().numpy(), aporx_function(x).clone().detach().numpy())
 
 if __name__ == '__main__':
-    fit_softplus_to_log_1_minus_cdf(load_from_chekpoint=True)
+    # fit_softplus_to_log_1_minus_cdf(load_from_chekpoint=True)
 
-    model_log_1_minus_cdf: Module = LogCdfEnsembleAproximation()
-    model_log_1_minus_cdf.load_state_dict(t.load(constants.LOG_CDF_APROXIMATION_CHECKPOINT))
+    model_log_cdf: Module = LogCdfEnsembleAproximation()
+    model_log_cdf.load_state_dict(t.load(constants.LOG_CDF_APROXIMATION_CHECKPOINT))
 
     log_cdf_plot(lambda x: t.log(to_tensor(norm.cdf(x))))
-    log_cdf_plot(lambda x: model_log_1_minus_cdf.forward(x))
+    log_cdf_plot(lambda x: model_log_cdf.forward(x))
 
     plt.show()

tobit/tobit_optimization.py
@@ -67,7 +67,9 @@ def tobit_mean_and_variance_reparametrization(intervals: List[p.interval.Interva
     delta, gamma = to_tensor(0, grad = True), to_tensor(1, grad = True)
     optimizer = t.optim.SGD([delta, gamma], lr=1e-1)
     patience = 5
-    for i in range(1000):
+    for i in range(10_000):
+        if i == 6035:
+            print('hi')
         prev_delta, prev_gamma = delta.clone(), gamma.clone()
         optimizer.zero_grad()
 
@@ -105,7 +107,7 @@ def tobit_mean_and_variance_reparametrization(intervals: List[p.interval.Interva
         else:
             patience = 5
 
-        if i % 100 == 0:
+        if i % 1 == 0:
             print(i, delta, gamma)
     print(i, delta, gamma)
     mean, std = delta / gamma, 1 / gamma
@@ -116,11 +118,11 @@ def plot_gausian(mean, std):
     plt.plot(x, norm.pdf(x, mean, std))
 
 if __name__ == '__main__':
-    no_tobit = np.array([10, 10, 10, 10, 30])
+    no_tobit = np.array([30, 30, 10])
     no_tobit_mean, no_tobit_std = norm.fit(no_tobit)
 
-    ic50 = [ p.closed(-p.inf, 10), p.closed(-p.inf, 10), p.closed(-p.inf, 10), p.closed(-p.inf, 10), p.singleton(30)]
-    mean, std = tobit_mean_and_variance_reparametrization(ic50, aproximation = False)
+    ic50 = [ p.closed(-p.inf, 30), p.closed(-p.inf, 30), p.singleton(10)]
+    mean, std = tobit_mean_and_variance_reparametrization(ic50, aproximation = True)
 
     print('No tobit mean', no_tobit_mean, 'std', no_tobit_std)
     plot_gausian(no_tobit_mean, no_tobit_std)